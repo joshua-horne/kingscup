bcbb7482978acf8fb41edac599f41f0d7ce6c61f
Josh Horne <joshua.lee.horne@gmail.com>
2023-05-07 14:00:48 +1200

    add styling to EnterPlayerNames

--- src/app/components/EnterPlayerNamesScreen.js
+++ src/app/components/EnterPlayerNamesScreen.js
@@ -1,6 +1,12 @@
 import React, {useState} from 'react';
-import {Box, Text, Button, CloseIcon, Input} from 'native-base';
-
+import {
+  Box,
+  Text,
+  Button,
+  CloseIcon,
+  Input,
+  KeyboardAvoidingView,
+} from 'native-base';
 import {useDispatch, useSelector} from 'react-redux';
 import {addPlayerName, removePlayerName} from '../store';
 
@@ -23,63 +29,82 @@ const EnterPlayerNamesScreen = ({navigation}) => {
   };
 
   const renderPlayerNames = () => {
-    return playerNames.map(name => {
-      return (
-        <Box
-          key={name}
-          flexDirection="row"
-          alignItems="center"
-          justifyContent="space-between"
-          p={2}
-          my={2}
-          borderRadius={4}
-          bg="coolGray.700">
-          <Text color="coolGray.50" fontSize={16}>
-            {name}
-          </Text>
-          <CloseIcon
-            size={4}
-            color="coolGray.50"
-            onPress={() => handleRemovePlayerName(name)}
-          />
-        </Box>
-      );
-    });
+    return (
+      <Box flexDirection="row" flexWrap="wrap" mt={2}>
+        {playerNames.map(name => (
+          <Box
+            key={name}
+            bg="coolGray.700"
+            borderRadius={4}
+            p={2}
+            mr={2}
+            mb={2}
+            flexDirection="row"
+            alignItems="center">
+            <Text color="coolGray.50" fontSize={16} mr={2}>
+              {name}
+            </Text>
+            <CloseIcon
+              size={4}
+              color="coolGray.50"
+              onPress={() => handleRemovePlayerName(name)}
+            />
+          </Box>
+        ))}
+      </Box>
+    );
   };
 
   return (
-    <Box bg="red.800" p={4} height="100%">
-      <Text color="coolGray.50" fontSize="3xl" mb={4}>
-        Enter Player Names:
-      </Text>
-      <Input
-        w="100%"
-        variant="underlined"
-        placeholder="Name"
-        value={newPlayerName}
-        onChangeText={text => setNewPlayerName(text)}
-        _focus={{borderColor: 'gray.700', borderWidth: 2}}
-        bg="white"
-        mb={4}
-        caretHidden={true}
-      />
-      <Button
-        colorScheme="white"
-        bg="gray.500"
-        onPress={handleAddPlayerName}
-        _pressed={{bg: 'gray.700'}}
-        mb={4}>
-        Add Player
-      </Button>
-      {renderPlayerNames()}
-      <Button
-        colorScheme="white"
-        bg="gray.500"
-        onPress={handleSubmit}
-        _pressed={{bg: 'gray.700'}}>
-        Start Game
-      </Button>
-    </Box>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <Box
+        bg="red.800"
+        p={4}
+        height="100%"
+        position="relative"
+        px={16}
+        pt={16}
+        pb={8}>
+        <Text color="coolGray.50" fontSize="3xl" mb={4}>
+          Enter Player Names:
+        </Text>
+        <Input
+          w="100%"
+          variant="underlined"
+          placeholder="Name"
+          value={newPlayerName}
+          onChangeText={text => setNewPlayerName(text)}
+          _focus={{borderColor: 'gray.700', borderWidth: 2}}
+          bg="white"
+          mb={4}
+          caretHidden={true}
+        />
+        <Button
+          colorScheme="white"
+          bg="gray.500"
+          onPress={handleAddPlayerName}
+          _pressed={{bg: 'gray.700'}}
+          mb={4}
+          alignSelf="flex-start">
+          Add Player
+        </Button>
+        <Box flex={1} justifyContent="flex-end" pb={4}>
+          {renderPlayerNames()}
+          <Button
+            colorScheme="white"
+            bg="gray.500"
+            onPress={handleSubmit}
+            _pressed={{bg: 'gray.700'}}
+            alignSelf="center"
+            px={6}
+            mt={4}>
+            Start Game
+          </Button>
+        </Box>
+      </Box>
+    </KeyboardAvoidingView>
   );
 };
 
